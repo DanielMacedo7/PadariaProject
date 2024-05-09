7be351673eb79644e978c29845ba521db3c0aefa
Daniel De Macedo <danielmcd172@gmail.com>
2024-05-09 00:26:46 -0300

    Implementação da classe Util que faz a integração com o postgres

--- src/main/java/com/example/padariaproject/Util/ConnectionDBA.java
+++ src/main/java/com/example/padariaproject/Util/ConnectionDBA.java
@@ -1,15 +1,58 @@
 package com.example.padariaproject.Util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 public class ConnectionDBA{
 
-    public final String DRIVER = "";
-    public final String URL = "";
-    public final String USER = "";
-    public final String PASSWORD = "";
+    //essa classe só irá conter métodos estáticos para não haver a necessidade de instacia-los.
 
-    public ConnectionDBA(){
+    // 3 ATRIBUTOS PRINCIPAIS PARA FAZER ESSA INTEGRAÇÃO COM O BANCO DE DADOS
+    public static final String URL = "jdbc:postgresql://localhost:5432/PadariaApp";
+    public static final String USER = "postgres";
+    public static final String PASS = "root";
+
+
+    //Abrindo a conexão com o banco de dados
+    public static Connection getConnection(){
+      try {
+          return DriverManager.getConnection(URL, USER, PASS);
+      }catch(SQLException ex ){
+          throw  new RuntimeException("Erro ao abrir conexão com o banco de dados", ex);
+      }
+    }
+
+    // Método para fechar conexão, fecha também a interface que executa instruções SQL e
+    // ResultSet que vai servir para buscar todos os nossos dados do objeto
+    public static void closeConnection(Connection connection, PreparedStatement statement, ResultSet resultSet) {
+        try {
+            if (connection != null) {
+                connection.close();
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Erro ao fechar conexão");
+        }
+
+        try{
+            if(statement != null){
+                statement.close();
+            }
+        }catch (SQLException e){
+            throw new RuntimeException("", e);
+        }
+
+        try{
+            if(resultSet != null){
+                resultSet.close();
+            }
+        }catch (SQLException e){
+            throw new RuntimeException("", e);
+        }
 
     }
+
 
 
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,6 +5,7 @@ module com.example.padariaproject {
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
     requires org.kordamp.bootstrapfx.core;
+    requires java.sql;
 
     opens com.example.padariaproject to javafx.fxml;
     exports com.example.padariaproject;
